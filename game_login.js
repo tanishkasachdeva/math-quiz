9c7d20a25fce00cc2097c948e39c5a97f9e6a923
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2021-02-15 15:34:24 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,110 @@
+function addName(){
+    player1_name= document.getElementById("player1_input_name").value;
+    player2_name= document.getElementById("player2_input_name").value;
+    localStorage.setItem("palyer1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    player_question_turn="
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    klol"
+    window.location="game_page.html";
+}
+function send(){
+    window.location=""
+}
